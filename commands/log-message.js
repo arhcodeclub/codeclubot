6b25ce1c1686fef59262a4402d7baf1381fd9f80
TetieWasTaken <tjvssr@gmail.com>
2023-04-09 17:58:15 +0200

    format: prettier

--- commands/log-message.js
+++ commands/log-message.js
@@ -1,19 +1,15 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('log-message')
-        .addStringOption((option) => 
-            option.setRequired(true)
-            .setName('log-message-content')
-            .setDescription('Message to log to console')
-        )
-		.setDescription('Logs message to discord bot console'),
-	async execute(interaction) {
+  data: new SlashCommandBuilder()
+    .setName('log-message')
+    .addStringOption((option) =>
+      option.setRequired(true).setName('log-message-content').setDescription('Message to log to console')
+    )
+    .setDescription('Logs message to discord bot console'),
+  async execute(interaction) {
+    console.log(interaction.options.get('log-message-content').value);
 
-        console.log(interaction.options.get('log-message-content').value);
-
-		await interaction.reply("Did thayut");
-        
-    }
+    await interaction.reply('Did thayut');
+  },
 };
--- commands/start-teamviewer.js
+++ commands/start-teamviewer.js
@@ -1,16 +1,14 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { fork, exec } = require("child_process");
-const fs = require("fs");
+const { fork, exec } = require('child_process');
+const fs = require('fs');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('start-teamviewer')
-		.setDescription('Starts teamviewer if it isn not open already.'),
-	async execute(interaction) {
+  data: new SlashCommandBuilder()
+    .setName('start-teamviewer')
+    .setDescription('Starts teamviewer if it isn not open already.'),
+  async execute(interaction) {
+    const prc = exec(`screen -dm teamviewer -X "teamviewer"`);
 
-        const prc = exec(`screen -dm teamviewer -X "teamviewer"`);
-
-		await interaction.reply("Did thayut");
-        
-    }
+    await interaction.reply('Did thayut');
+  },
 };

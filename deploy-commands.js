fde766d5bd26db253876124d047575c5899bdf5c
Distractech <distractech.dev@gmail.com>
2023-05-04 15:22:47 +0200

    combined deploying and running bot

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,28 +1,28 @@
 const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
-const dotenv = require('dotenv');
 
-dotenv.config();
+const nconf = require("nconf");
 
-const guildId = process.env.GUILDID;
-const clientId = process.env.CLIENTID;
-const token = process.env.TOKEN;
+async function run() {
 
-const commands = [];
-// Grab all the command files from the commands directory you created earlier
-const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+    const guildId = nconf.get("GUILDID");
+    const clientId = nconf.get("CLIENTID");
+    const token = nconf.get("TOKEN");
 
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
-}
+    const commands = [];
+    // Grab all the command files from the commands directory you created earlier
+    const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+    
+    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        commands.push(command.data.toJSON());
+    }
+    
+    // Construct and prepare an instance of the REST module
+    const rest = new REST().setToken(token);
+    
 
-// Construct and prepare an instance of the REST module
-const rest = new REST().setToken(token);
-
-// and deploy your commands!
-(async () => {
     try {
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
@@ -38,4 +38,7 @@ const rest = new REST().setToken(token);
         // And of course, make sure you catch and log any errors!
         console.error(error);
     }
-})();
+
+}
+
+module.exports = { run };
--- index.js
+++ index.js
@@ -1,75 +1,22 @@
-// Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 const dotenv = require('dotenv');
-const fs = require('node:fs');
-const path = require('node:path');
+// https://github.com/indexzero/nconf
+const nconf = require("nconf");
+
+const bot = require("./bot.js");
+const deploy = require("./deploy-commands.js");
 
 dotenv.config();
+// initialize nconf to use argv over environment variables
+nconf.argv().env();
 
-const token = process.env.TOKEN;
-
-// Create a new client instance
-const client = new Client({ intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMessageReactions
-] });
-
-client.commands = new Collection();
-
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ('data' in command && 'execute' in command) {
-        client.commands.set(command.data.name, command);
-    } else {
-        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-    }
+switch (nconf.get("ROLE")) {
+    case "REFRESH":
+        deploy.run();
+        break;
+    case "BOT":
+        bot.run();
+        break;
+    default:
+        console.log("No `ROLE` parameter given, terminating");
+        break;
 }
-
-async function commandError(interaction) {
-    if (interaction.replied) return;
-
-    const response = await interaction.reply({
-        content: 'There was an error while executing this command!',
-        ephemeral: true,
-    });
-}
-
-client.on(Events.InteractionCreate, async (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        
-        commandError(interaction);
-
-        return;
-    }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        
-        commandError(interaction);
-    }
-});
-
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, (c) => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-client.on(Events.Error, console.error);
-
-// Log in to Discord with your client's token
-client.login(token);

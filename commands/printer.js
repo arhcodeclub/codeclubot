9f321469397036564bbb647fb0ef0f3ae1c78200
Distractech <distractech.dev@gmail.com>
2023-10-09 17:28:08 +0200

    Added print command

---
+++ commands/printer.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fetch = require("fetch");
+const nconf = require("nconf");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('showlatestimage')
+        .setDescription('Shows last image taken by picamera'),
+    async execute(interaction) {
+
+        // TODO get image from url endpoint
+
+    },
+};
